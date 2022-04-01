ec28580695617c673180f85e20c7160c68e8537f
Alex <alex.m3101@gmail.com>
2022-04-02 01:30:34 +0300

    - добавил окно у ученика (изменение пароля, присоединение к группе, результаты); - добавил окно у админа (все пользователи); - у учителя пока тихо; - кнопку "Опции" сделал только одну в общем меню; - сообщение когда пользователь зашел в аккаунт сделал сверху; - сообщение об ошибках сделал снизу - надо сделать скрипт для этого.

--- Assets/Scripts/MenuScr/Scr_Menu.cs
+++ Assets/Scripts/MenuScr/Scr_Menu.cs
@@ -8,7 +8,94 @@ public class Scr_Menu : MonoBehaviour
     public GameObject Input;
     public GameObject Registration;
     public GameObject Menu_start;
+    public GameObject Menu_Srudent;
+    public GameObject Menu_Teacher;
+    public GameObject Menu_Admin;
     public GameObject About;
+    public GameObject Opt;
+    public GameObject Change_pass_Student;
+    public GameObject Join_Student;
+    public GameObject Results_Student;
+    public GameObject All_users_Admin;
+    public GameObject Results_Teachert;
+
+    public void Menu_Srudent_in()
+    {
+        Menu_Srudent.SetActive(true);
+    }
+    public void Menu_Srudent_out()
+    {
+        Menu_Srudent.SetActive(false);
+    }
+    public void Menu_Teacher_in()
+    {
+        Menu_Teacher.SetActive(true);
+    }
+    public void Menu_Teacher_out()
+    {
+        Menu_Teacher.SetActive(false);
+    }
+    public void Menu_Admin_in()
+    {
+        Menu_Admin.SetActive(true);
+    }
+    public void Menu_Admin_out()
+    {
+        Menu_Admin.SetActive(false);
+    }
+    public void Optt_in()
+    {
+        Opt.SetActive(true);
+    }
+    public void Opt_out()
+    {
+        Opt.SetActive(false);
+    }
+
+    public void Change_pass_Student_in()
+    {
+        Change_pass_Student.SetActive(true);
+    }
+    public void Change_pass_Student_out()
+    {
+        Change_pass_Student.SetActive(false);
+    }
+
+    public void Join_Student_in()
+    {
+        Join_Student.SetActive(true);
+    }
+    public void Join_Student_out()
+    {
+        Join_Student.SetActive(false);
+    }
+
+    public void Results_Student_in()
+    {
+        Results_Student.SetActive(true);
+    }
+    public void Results_Student_out()
+    {
+        Results_Student.SetActive(false);
+    }
+
+    public void All_users_Admin_in()
+    {
+        All_users_Admin.SetActive(true);
+    }
+    public void All_users_Admin_out()
+    {
+        All_users_Admin.SetActive(false);
+    }
+
+    public void Results_Teachert_in()
+    {
+        Results_Teachert.SetActive(true);
+    }
+    public void Results_Teachert_out()
+    {
+        Results_Teachert.SetActive(false);
+    }
 
     public void Input_in()
     {
