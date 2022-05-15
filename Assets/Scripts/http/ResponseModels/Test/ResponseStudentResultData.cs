26797a5afda37f36bd1970e2dc5af524217b387f
VavilonBase <VavilonBase@mail.ru>
2022-05-16 01:24:44 +0300

    Вместо получения всех полных результатов ученика за тест, теперь можно получить один полный результат за тест по id результата

--- Assets/Scripts/http/ResponseModels/Test/ResponseStudentResultData.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System.Collections.Generic;
-
-public class ResponseStudentResultData
-{
-    public int resultId { get; set; } // Идентификатор результата
-    public int totalScores { get; set; } // Очки за тест по текущему результату
-    public List<ResponseFullResult> results { get; set; } // Результаты
-}
--- Assets/Scripts/http/ResponseModels/Test/ResponseStudentResultWithRightAnswer.cs
+++ Assets/Scripts/http/ResponseModels/Test/ResponseStudentResultWithRightAnswer.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 public class ResponseStudentResultWithRightAnswer
 {
     public int userId { get; set; } // Идентификатор ученика
-    public int testId { get; set; } // Идентификатор результата
-    public List<ResponseStudentResultData> resultsData { get; set; } // Результаты
+    public int testId { get; set; } // Идентификатор теста
+    public int resultId { get; set; } // Идентификатор результата
+    public int totalScores { get; set; } // Количество очков за тест
+    public List<ResponseFullResult> resultsData { get; set; } // Результаты
 }
